1d4e9c6fa0f45032cd9d85b891a7f18308610270
Shoki Yamamoto <e185711@ie.u-ryukyu.ac.jp>
2019-01-21 19:13:20 +0900

    2nd commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e185711_e185713/Game.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e185711_e185713;
+
+public class Game {
+    Game(){
+        System.out.println("PlayBall!");
+    }
+}
--- src/main/java/jp/ac/uryukyu/ie/e185711_e185713/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185711_e185713/Main.java
@@ -1,7 +1,8 @@
 package jp.ac.uryukyu.ie.e185711_e185713;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("PlayBall!");
+    public static void main(String[] args) {
+        Game gm = new Game();
+
     }
 }
